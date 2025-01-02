2308be95231dd7475d06275dd9031425fe246211
char5742 <fujino@stellarcode.jp>
2025-01-02 21:54:13 +0900

    feat: 🎸 アイテムリストハンドラーを追加 追加されたアイテムリストハンドラーは、アイテムのリストを取得し、JSON形式で返します。

---
+++ internal/item/handler/item_list_handler.go
@@ -0,0 +1,88 @@
+//go:generate mockgen -source=$GOFILE -destination=mock_$GOFILE -package=$GOPACKAGE
+package handler
+
+import (
+	"char5742/ecsite-sample/internal/item/infra"
+	"char5742/ecsite-sample/pkg/db"
+	"encoding/json"
+	"log"
+	"net/http"
+	"time"
+)
+
+type ItemListHandler interface {
+	Handler(w http.ResponseWriter, r *http.Request)
+}
+
+type ItemListHandlerImpl struct {
+	query infra.ItemListQuery
+	db    db.DB
+}
+
+func NewItemListHandler(query infra.ItemListQuery, db db.DB) ItemListHandler {
+	return &ItemListHandlerImpl{query: query, db: db}
+}
+
+func (h *ItemListHandlerImpl) Handler(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	tx, err := h.db.BeginTx(ctx, nil)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	defer tx.Commit()
+	items, err := h.query.ItemList(ctx, tx)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	var t []ItemResponse
+	for _, item := range items {
+		var itemResponse ItemResponse
+		itemResponse.ID = item.ID.UUID.String()
+		itemResponse.Description = item.Description
+		itemResponse.Price = item.Price
+		itemResponse.BirthDay = item.Birthday
+		itemResponse.Image = item.Image
+		itemResponse.IsDeleted = item.IsDeleted
+		itemResponse.Gender = item.Gender.Name
+		itemResponse.Breed.ID = item.Breed.ID.UUID.String()
+		itemResponse.Breed.Name = item.Breed.Name
+		itemResponse.Color.ID = item.Color.ID.UUID.String()
+		itemResponse.Color.Name = item.Color.Name
+		t = append(t, itemResponse)
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(t); err != nil {
+		log.Println(err)
+	}
+}
+
+type ItemResponse struct {
+	ID string `json:"id"`
+	// 詳細
+	Description string `json:"description"`
+	// 価格
+	Price int `json:"price"`
+	// 誕生日
+	BirthDay time.Time `json:"birthDay"`
+	// 画像パス
+	Image string `json:"image"`
+	// 削除フラグ
+	IsDeleted bool `json:"deleted"`
+	// 性別
+	Gender string `json:"gender"`
+	// 種別
+	Breed struct {
+		ID   string `json:"id"`
+		Name string `json:"name"`
+	} `json:"breed"`
+	// 色
+	Color struct {
+		ID   string `json:"id"`
+		Name string `json:"name"`
+	} `json:"color"`
+}
---
+++ internal/item/handler/item_list_handler_test.go
@@ -0,0 +1,68 @@
+package handler
+
+import (
+	"char5742/ecsite-sample/internal/item/domain"
+	"char5742/ecsite-sample/internal/item/infra"
+	"char5742/ecsite-sample/pkg/db"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestItemListHandler_Handler(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	mockQuery := infra.NewMockItemListQuery(ctrl)
+	mockDB := db.NewMockDB(ctrl)
+	mockTX := db.NewMockTX(ctrl)
+	handler := NewItemListHandler(mockQuery, mockDB)
+	t.Log("TestItemListHandler_Handler")
+	// Mock data
+	items := []domain.Item{
+		{
+			ID:          domain.NewItemID("44444444-4444-4444-4444-444444444444"),
+			Description: "Item 1",
+			Price:       100,
+			Birthday:    time.Now(),
+			Image:       "image1.png",
+			IsDeleted:   false,
+			Gender:      domain.Gender{Name: "Male"},
+			Breed: domain.Breed{ID: domain.NewBreedID("44444444-4444-4444-4444-444444444444"),
+				Name: "Breed 1"},
+			Color: domain.Color{ID: domain.NewColorID("44444444-4444-4444-4444-444444444444"),
+				Name: "Color 1"},
+		},
+	}
+
+	mockQuery.EXPECT().ItemList(gomock.Any(), gomock.Any()).Return(items, nil)
+	mockDB.EXPECT().BeginTx(gomock.Any(), gomock.Any()).Return(mockTX, nil)
+	mockTX.EXPECT().Commit().Return(nil)
+
+	req, err := http.NewRequest("GET", "/items", nil)
+	assert.NoError(t, err)
+
+	rr := httptest.NewRecorder()
+	handler.Handler(rr, req)
+
+	assert.Equal(t, http.StatusOK, rr.Code)
+	assert.Equal(t, "application/json; charset=utf-8", rr.Header().Get("Content-Type"))
+
+	var response []ItemResponse
+	t.Logf("%s", rr.Body.String())
+	err = json.NewDecoder(rr.Body).Decode(&response)
+	assert.NoError(t, err)
+	assert.Len(t, response, 1)
+	assert.Equal(t, "44444444-4444-4444-4444-444444444444", response[0].ID)
+	assert.Equal(t, "Item 1", response[0].Description)
+	assert.Equal(t, 100, response[0].Price)
+	assert.Equal(t, "image1.png", response[0].Image)
+	assert.Equal(t, "Male", response[0].Gender)
+	assert.Equal(t, "44444444-4444-4444-4444-444444444444", response[0].Breed.ID)
+	assert.Equal(t, "Breed 1", response[0].Breed.Name)
+	assert.Equal(t, "44444444-4444-4444-4444-444444444444", response[0].Color.ID)
+	assert.Equal(t, "Color 1", response[0].Color.Name)
+}
